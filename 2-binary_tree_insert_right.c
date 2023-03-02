6d0fe23be10691f2d64bc08fab7ab36f9bcd41b1
Arden007 <cloveryuno664@gmail.com>
2023-03-02 21:30:45 +0200

    task complete betty check

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,25 +8,25 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = NULL;
+binary_tree_t *node = NULL;
 
-    if (parent == NULL)
-        return (NULL);
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
+if (parent == NULL)
+return (NULL);
+node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    if (parent->right == NULL)
-        parent->right = node;
-    else
-    {
-        node->right = parent->right;
-        parent->right = node;
-        node->right->parent = node;
-    }
-    return (node);
+node->n = value;
+node->parent = parent;
+node->left = NULL;
+node->right = NULL;
+if (parent->right == NULL)
+parent->right = node;
+else
+{
+node->right = parent->right;
+parent->right = node;
+node->right->parent = node;
+}
+return (node);
 }
