7e3a086717e8e086f0ed4140985a811e55acc584
Arden007 <cloveryuno664@gmail.com>
2023-03-02 21:31:57 +0200

    task complete betty check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,5 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_node - Creation of a binary tree node.
- *@parent: points to the parent of the node.
- *@value: the value for the node created.
- * Return:the function returns a pointer to the node created on success and NULL when errors occur
- */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
